9022fd459d65b79051e2b50808384ac657f8cd1f
Ricky Ariansyah <rickyarians@outlook.com>
2024-11-09 02:09:19 +0700

    try some view in swift ui

--- splitBills/ContentView.swift
+++ splitBills/ContentView.swift
@@ -9,20 +9,45 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-   
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello World")
-        }
-        .padding()
-    }
-
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    @State private var name = ""
+    @State private var counting = 0
     
-
-   
+    var body: some View {
+        NavigationStack{
+            Form {
+                Section(header: Text("Settingan Rata Kiris")) {
+                    TextField("Enter your name", text: $name)
+                        .onChange(of: name) { oldValue, newValue in
+                            print("kocak \(newValue)")
+                        }
+                    Text("\(name)")
+                   Text("\(selectedStudent)")
+                }
+                Section {
+                    Picker("Select your student", selection: $selectedStudent) {
+                        ForEach(students, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                Section {
+                    ForEach(0..<10) {
+                        number in
+                        Text("Hello World! \(String(number + 1))")
+                    }
+                }
+                
+                Button("tap count \(String(counting))") {
+                    counting += 1
+                }
+            }.navigationTitle("SwiftUI")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+            
+       
+    }
 }
 
 
